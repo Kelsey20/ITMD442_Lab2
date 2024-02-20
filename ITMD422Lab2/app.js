cb224955cee7e233ced7d72b6d09a1afa65d4b95
Xiaojie Luo <xluo30@hawk.iit.edu>
2024-02-20 10:52:17 -0600

    create package-lock jason file

--- ITMD422Lab2/app.js
+++ ITMD422Lab2/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//add new routers
+var contactsRouter = require('./routes/contacts');
 
 var app = express();
 
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//add new app use
+app.use('/contacts', contactsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
